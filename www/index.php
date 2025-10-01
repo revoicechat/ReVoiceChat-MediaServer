e7bb1dce1e3778514320e61720e2377245a3623e
Raphi_55 <35998957+Raphi-Sch@users.noreply.github.com>
2025-10-01 11:52:52 +0200

    refactor(emote): Seperate emoji from index

--- www/index.php
+++ www/index.php
@@ -1,6 +1,5 @@
 <?php
-
-use LDAP\Result;
+require_once 'src/file_access.php';
 
 const CONTENT_TYPE_APPLICATION_JSON = "Content-Type: application/json";
 
@@ -19,26 +18,6 @@ switch ($_SERVER["REQUEST_METHOD"]) {
             break;
         }
 
-        if (isset($_GET['emojis-global'])) {
-            if ($_GET['emojis-global'] == "all") {
-                get_emojis_global_all();
-                break;
-            } else {
-                get_file('emojis/global', $_GET['emojis-global']);
-                break;
-            }
-        }
-
-        if (isset($_GET['emojis']) && !empty($_GET['emojis'])) {
-            // Direct file access
-            if ($_GET['emojis'] == 'all') {
-                get_emojis_all();
-            } else {
-                get_file('emojis', $_GET['emojis']);
-            }
-            break;
-        }
-
         http_response_code(400);
         break;
 
@@ -50,15 +29,6 @@ switch ($_SERVER["REQUEST_METHOD"]) {
 
         if (isset($_GET['profiles']) && !empty($_GET['profiles'])) {
             post_profile_upload($_GET['profiles']);
-            break;
-        }
-
-        if (isset($_GET['emojis']) && !empty($_GET['emojis'])) {
-            if ($_GET['emojis'] == "bulk") {
-                options_file_bulk('emojis', $body);
-            } else {
-                options_file('emojis', $_GET['emojis']);
-            }
             break;
         }
 
@@ -76,78 +46,6 @@ switch ($_SERVER["REQUEST_METHOD"]) {
 
 exit;
 
-function get_file($where, $name)
-{
-    $file = dirname(__FILE__) . "/data/$where/$name";
-
-    if (!file_exists($file)) {
-        if ($where == "profiles" && $name != "default-avatar") {
-            get_file("profiles", "default-avatar");
-            exit;
-        }
-        http_response_code(404);
-        exit;
-    }
-
-    $type = mime_content_type($file);
-
-    header("Content-Disposition: inline");
-    header("Content-Type: $type");
-    readfile($file);
-
-    exit;
-}
-
-function get_emojis_all()
-{
-    $list = scandir(dirname(__FILE__) . "/data/emojis/");
-    $result = array_values(array_diff($list, [".", "..", ".keep"]));
-
-    header(CONTENT_TYPE_APPLICATION_JSON);
-    echo json_encode($result);
-
-    exit;
-}
-
-function get_emojis_global_all()
-{
-    $list = scandir(dirname(__FILE__) . "/data/emojis/global/");
-    $list = array_values(array_diff($list, [".", "..", ".keep"]));
-
-    header(CONTENT_TYPE_APPLICATION_JSON);
-    echo json_encode($list);
-
-    exit;
-}
-
-function options_file($where, $name)
-{
-    $file = dirname(__FILE__) . "/data/$where/$name";
-
-    if (file_exists($file)) {
-        http_response_code(200);
-        exit;
-    } else {
-        http_response_code(204);
-        exit;
-    }
-}
-
-function options_file_bulk($where, $names)
-{
-    $result = [];
-
-    foreach ($names as $name) {
-        $file = dirname(__FILE__) . "/data/$where/$name";
-        $result[$name] = file_exists($file);
-    }
-
-    http_response_code(200);
-    header(CONTENT_TYPE_APPLICATION_JSON);
-    echo json_encode($result);
-
-    exit;
-}
 
 function post_profile_upload($id)
 {
---
+++ www/src/file_access.php
@@ -0,0 +1,52 @@
+<?php
+
+function get_file($where, $name)
+{
+    $file = __DIR__ . "/data/$where/$name";
+
+    if (!file_exists($file)) {
+        if ($where == "profiles" && $name != "default-avatar") {
+            get_file("profiles", "default-avatar");
+            exit;
+        }
+        http_response_code(404);
+        exit;
+    }
+
+    $type = mime_content_type($file);
+
+    header("Content-Disposition: inline");
+    header("Content-Type: $type");
+    readfile($file);
+
+    exit;
+}
+
+function options_file($where, $name)
+{
+    $file = dirname(__FILE__) . "/data/$where/$name";
+
+    if (file_exists($file)) {
+        http_response_code(200);
+        exit;
+    } else {
+        http_response_code(204);
+        exit;
+    }
+}
+
+function options_file_bulk($where, $names)
+{
+    $result = [];
+
+    foreach ($names as $name) {
+        $file = dirname(__FILE__) . "/data/$where/$name";
+        $result[$name] = file_exists($file);
+    }
+
+    http_response_code(200);
+    header(CONTENT_TYPE_APPLICATION_JSON);
+    echo json_encode($result);
+
+    exit;
+}
