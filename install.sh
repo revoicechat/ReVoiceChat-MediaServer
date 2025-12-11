45bc18f602ffff788a96374375c304338ae0df78
Raphael <raphael.scharff@gmail.com>
2025-12-11 23:19:07 +0100

    docs(install): update echo

--- install.sh
+++ install.sh
@@ -7,16 +7,16 @@ function pause(){
 echo "ReVoiceChat-MediaServer installer"
 pause
 
-echo "Installing Apache2 ..."
+echo "Installing Apache2 and PHP ..."
 sudo apt-get install apache2-utils apache2 php libapache2-mod-php php-json php-zip php-curl php-gd -y
 
-echo "Configuring Apache2 service ..."
+echo "Configuring Apache2 ..."
 sudo a2enmod headers
 sudo a2enmod rewrite
 sudo cp rvc_media.exemple.conf /etc/apache2/sites-available/rvc_media.conf
 sudo a2ensite rvc_media.conf
 
-echo "Enabling Apache2 service ..."
+echo "Enabling Apache2 ..."
 sudo systemctl enable apache2
 sudo systemctl restart apache2
 
@@ -28,3 +28,5 @@ sudo chgrp www-data /var/log/rvc
 echo "Changing permissions on 'data' directory ..."
 sudo chown www-data ./www/data
 sudo chgrp www-data ./www/data
+
+echo "Done."
